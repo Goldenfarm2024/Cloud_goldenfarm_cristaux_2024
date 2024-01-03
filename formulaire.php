5e0bdc6ac636f5a50b01ea9c95eedd309d8a777f
Cloud.goldenfarm2024 <155451124+Goldenfarm2024@users.noreply.github.com>
2024-01-02 20:16:25 -0900

    Update formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -6,12 +6,21 @@
     $username = $_POST['username'];
     $email = $_POST['email'];
     $mot_de_pass = $_POST['mot_de_pass'];
+   
+    function traiter_formulaire() {
+     
+    }
+    // Traitement du formulaire
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      // code...
+    }
 
     // Créez une connexion à la base de données
     $dsn =  'mysql:host=db4free.net;dbname=goldenfarm';
     $utilisateur = 'admin_users';
     $password = 'Base3624@';
     $pdo = new PDO($dsn, $utilisateur, $password);
+
 
     // Insérez les données dans la base de données
     $requete = "INSERT INTO Tb_goldenfarm (username, email, mot_de_pass) VALUES ('$username', '$email', '$mot_de_pass')";
